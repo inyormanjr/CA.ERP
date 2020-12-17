42e013850eb96440316402a2d592d51097725d01
Vic <v.g.albonian@gmail.com>
2020-12-17 21:43:49 +0800

    Added Health check test

---
+++ Test/CA.ERP.WebApp.Test.Integration/Tests/HealthTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CA.ERP.WebApp.Test.Integration.Tests
+{
+    public class HealthTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private WebApplicationFactory<Startup> _factory;
+        private HttpClient _client;
+
+        public HealthTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+
+        }
+        [Fact]
+        public async Task ShouldReturnHealthy()
+        {
+            var response = await _client.GetAsync("/health");
+            var health = await response.Content.ReadAsStringAsync();
+            Assert.Equal("Healthy", health);
+        }
+    }
+}
