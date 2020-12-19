5527b0d3a0deb601d2ab886c150c18fa0355c1c6
Vic <v.g.albonian@gmail.com>
2020-12-19 22:08:01 +0800

    Added common lib and remove old CA.ERP.Lib

---
+++ CA.ERP.Common/Extensions/NullChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA.ERP.Common.Extensions
+{
+    public static class NullChecker
+    {
+        public static bool IsNull(this object obj)
+        {
+            return obj == null ? true : false;
+        }
+
+        public static bool IsNotNull(this object obj)
+        {
+            return !obj.IsNull();
+        }
+
+        public static void ThrowIfNullArgument(this object obj, string parameterName)
+        {
+            if (obj.IsNull())
+            {
+                throw new ArgumentNullException($"Argument {parameterName} is null.");
+            }
+        }
+    }
+}
