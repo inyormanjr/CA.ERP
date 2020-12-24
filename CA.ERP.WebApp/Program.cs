0a316b11a2c91244e1e39df4fd7918d82967b7bd
Vic <v.g.albonian@gmail.com>
2020-12-24 19:05:02 +0800

    added local.json

--- CA.ERP.WebApp/Program.cs
+++ CA.ERP.WebApp/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,9 +19,15 @@ namespace CA.ERP.WebApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((host, builder) => {
+                    var localJsonPath = Path.Combine(System.AppContext.BaseDirectory, $"appsettings.{host.HostingEnvironment.EnvironmentName}.local.json");
+                    if (File.Exists(localJsonPath))
+                    {
+                        builder.AddJsonFile(localJsonPath);
+                    }
+                })
                 .ConfigureLogging((hostingContext, builder) =>
                 {
-                    //log per run
                     builder.AddFile("logs/ca-erp-{Date}.log");
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
