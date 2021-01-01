f14fe7de076b83cffa5555438a894e667fba72f9
Vic <v.g.albonian@gmail.com>
2021-01-01 17:14:47 +0800

    change barcode generation. added unit test for barcode closes #136

---
+++ CA.ERP.Domain/Helpers/DateTimeHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA.ERP.Domain.Helpers
+{
+    public class DateTimeHelper : IDateTimeHelper
+    {
+        public DateTime GetCurrentDateTime()
+        {
+            return DateTime.Now;
+        }
+
+        public DateTimeOffset GetCurrentDateTimeOffset()
+        {
+            return DateTimeOffset.Now;
+        }
+    }
+}
