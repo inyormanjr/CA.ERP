6683924e9615b69cd881c9baef9c976bd71f1136
Vic <v.g.albonian@gmail.com>
2021-01-17 02:05:45 +0800

    delete duplicate files

--- Misc/CA.ERP.JSReport.Studio/jsreport/data/shared/global-helpers.js/content.js
+++
@@ -1,8 +0,0 @@
-function formatMoney(money){
-    var formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'PHP',
-        currencyDisplay: 'symbol',
-    });
-    return formatter.format(money).replace("PHP", "₱");;
-}
