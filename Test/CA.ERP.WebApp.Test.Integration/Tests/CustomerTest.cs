c72a473ec7f0bba199217591b9493f5ba8778a3b
Vic0111 <v.g.albonian@gmail.com>
2021-01-23 01:39:26 +0800

    Added get one customer

--- Test/CA.ERP.WebApp.Test.Integration/Tests/CustomerTest.cs
+++ Test/CA.ERP.WebApp.Test.Integration/Tests/CustomerTest.cs
@@ -151,5 +151,29 @@ namespace CA.ERP.WebApp.Test.Integration.Tests
             }
         }
 
+        [Fact]
+        public async Task ShouldGetOneCustomerSuccess_Ok()
+        {
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetService<CADataContext>();
+                var random = new Random();
+                var customer = dbContext.Customers.ToList().OrderBy(c => random.Next()).FirstOrDefault();
+            
+                var searchForFirstname = customer.FirstName.Substring(0, 3);
+                var searchForLastname = customer.LastName.Substring(0, 3);
+                var response = await _client.GetAsync($"api/Customer/{customer.Id}");
+
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+                var content =  await response.Content.ReadAsAsync<Dto.Customer.CustomerView>();
+                content.LastName.Should().Be(customer.LastName);
+                content.FirstName.Should().Be(customer.FirstName);
+                content.Address.Should().Be(customer.Address);
+                content.CoMaker.Should().Be(customer.CoMaker);
+                content.Id.Should().Be(customer.Id);
+            }
+        }
+
     }
 }
