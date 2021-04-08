9125974af740a5294f389b409ea057bf77220270
Vicente <v.g.albonian@gmail.com>
2021-04-09 00:14:51 +0800

    added mud blazor

--- CA.ERP.WebApp.Blazor/Program.cs
+++ CA.ERP.WebApp.Blazor/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -34,6 +35,8 @@ namespace CA.ERP.WebApp.Blazor
                 // For more information, see https://aka.ms/blazor-standalone-auth
                 builder.Configuration.Bind("Identity", options.ProviderOptions);
             });
+
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
