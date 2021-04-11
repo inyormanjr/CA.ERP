76e1d03770c0d4ed5c1d1f2952af9c4d4d236b7e
Vicente <v.g.albonian@gmail.com>
2021-04-11 21:51:32 +0800

    fix ignore file

---
+++ CA.Identity/Data/Migrations/20210411125523_AddBranchNameAndCode.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CA.Identity.Data.Migrations
+{
+    public partial class AddBranchNameAndCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BranchCode",
+                table: "UserBranches",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BranchName",
+                table: "UserBranches",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "AspNetUserRoles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUserRoles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoleId1",
+                table: "AspNetUserRoles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_ApplicationUserId",
+                table: "AspNetUserRoles",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId1",
+                table: "AspNetUserRoles",
+                column: "RoleId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetRoles_RoleId1",
+                table: "AspNetUserRoles",
+                column: "RoleId1",
+                principalTable: "AspNetRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserRoles",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetRoles_RoleId1",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_ApplicationUserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_RoleId1",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "BranchCode",
+                table: "UserBranches");
+
+            migrationBuilder.DropColumn(
+                name: "BranchName",
+                table: "UserBranches");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId1",
+                table: "AspNetUserRoles");
+        }
+    }
+}
