23b3ca47fed259929cb84f46adbcb5c34ca2f9df
Vic0222 <v.g.albonian@gmail.com>
2021-04-17 14:05:14 +0800

    deleted identity spa

--- CA.Identity/Startup.cs
+++ CA.Identity/Startup.cs
@@ -108,11 +108,6 @@ namespace CA.Identity
 
             services.AddControllersWithViews();
             services.AddRazorPages();
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
 
             services.AddMvc();
 
@@ -138,12 +133,8 @@ namespace CA.Identity
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
-            if (!env.IsDevelopment())
-            {
-                app.UseSpaStaticFiles();
-            }
 
             app.UseRouting();
 
@@ -174,18 +165,6 @@ namespace CA.Identity
 
             });
 
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
-            });
 
 
         }
