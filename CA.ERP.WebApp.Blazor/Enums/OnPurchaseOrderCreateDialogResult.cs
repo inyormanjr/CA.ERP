730c9b1ad72073bbaa3378a8e7acd0296ee26273
Vic0222 <v.g.albonian@gmail.com>
2021-05-01 16:59:45 +0800

    Update after saving Purchase Order closes #316

---
+++ CA.ERP.WebApp.Blazor/Enums/OnPurchaseOrderCreateDialogResult.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CA.ERP.WebApp.Blazor.Enums
+{
+    public enum OnPurchaseOrderCreateDialogResult : int
+    {
+        List = 0,
+        Print = 1,
+        CreateAnother = 2
+    }
+}
